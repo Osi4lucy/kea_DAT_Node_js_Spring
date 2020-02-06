6057fa4c7d344e7646b34e25df2f5315f0bbf7c5
Osi4lucy <soloosilamah@yahoo.com>
2020-02-06 09:51:39 +0100

    Added the first exercise

---
+++ Week_1/Week_1/1.Intro/1.Intro/Intro_Exercises/1.Basic.js
@@ -0,0 +1,12 @@
+var firstName = "Solomon";
+var lastName = "Anders";
+console.log("My first name is ", firstName, "and my last name is ", lastName);
+
+//Exercise 2
+var year = "2017";
+var number = 1;
+
+var newYear = Number(year) + 1;
+console.log(newYear);
+
+
